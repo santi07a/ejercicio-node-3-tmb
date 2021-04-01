d7a63325a338b8b317956ae6c513ccb6d2b427d4
Santi Arnau <santi07a@gmail.com>
2021-04-01 19:00:13 +0200

    bugfix get linea dentro de app.get

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const morgan = require("morgan");
 const fetch = require("node-fetch");
 const debug = require("debug");
 
-const getLinea = () => fetch(process.env.TMB_API_LINEAS)
-  .then(resp => resp.json())
-  .then(datos => datos.features.map(linea => ({
-    id: linea.properties.CODI_LINIA,
-    linea: linea.properties.NOM_LINIA,
-    descripcion: linea.properties.DESC_LINIA
-  })));
-
 program.option("-p, --puerto <puerto>", "Puerto para el servidor");
 program.parse(process.argv);
 const options = program.opts();
@@ -30,8 +22,17 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 
 app.get("/metro/lineas", (req, res, next) => {
-  res.json(getLinea());
+  const getLinea = () => fetch(process.env.TMB_API_LINEAS)
+    .then(resp => resp.json())
+    .then(datos => res.json(
+      datos.features.map(linea => ({
+        id: linea.properties.CODI_LINIA,
+        linea: linea.properties.NOM_LINIA,
+        descripcion: linea.properties.DESC_LINIA
+      }))
+    )); getLinea();
 });
+
 app.use((req, res, next) => {
   res.status(404).json({ error: true, mensaje: "Recurso no encontrado" });
 });
